48611226017642bea7c88f7b350ed9b9c2e27baf
Bryan Alanoca <bryanalanocalopez@gmail.com>
2026-04-27 11:26:58 -0400

    parcial

--- 2_Parcial/app/layout.tsx
+++ 2_Parcial/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "Jira Helper Cards",
-  description: "2do Parcial - Programacion Avanzada",
-};
+
 
 export default function RootLayout({
   children,
